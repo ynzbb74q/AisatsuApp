3436a92625e9ea091409471c9d4b99e035ea78b2
ynzbb74q <ochiai.takayuki@fujitsu.com>
2019-07-09 16:09:49 +0900

    TimePickerDialog上で設定した値から、あいさつ文言を特定できるように修正

--- app/src/main/java/jp/techacademy/takayuki/ochiai/aisatsuapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/takayuki/ochiai/aisatsuapp/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 import android.app.TimePickerDialog
 import androidx.appcompat.app.AlertDialog
+import android.widget.TextView.BufferType;
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,7 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        buttonTimePickerDialog.setOnClickListener{
+        buttonTimePickerDialog.setOnClickListener {
             showTimePickerDialog()
         }
     }
@@ -21,14 +22,16 @@ class MainActivity : AppCompatActivity() {
     private fun showTimePickerDialog() {
         val inputHour: String? = editHour.text.toString()
         val inputMinute: String? = editMinute.text.toString()
+        var setHour: Int = 0
+        var setMinute: Int = 0
 
         // 整数チェック用パターン
         val regex = Regex("\\d+")
 
-        // 入力値チェック(null, 整数, 範囲)
-        if (inputHour.isNullOrEmpty() || inputMinute.isNullOrEmpty()
-            || !regex.matches(inputHour) || !regex.matches(inputMinute)
-            || !(inputHour.toInt() in 0..23 && inputMinute.toInt() in 0..59)) {
+        // 入力値チェック(整数, 範囲)
+        if (!(inputHour.isNullOrEmpty() || inputMinute.isNullOrEmpty())
+            && (!(regex.matches(inputHour) && regex.matches(inputMinute))
+                    || !(inputHour.toInt() in 0..23 && inputMinute.toInt() in 0..59))) {
             // エラー通知ダイアログ表示
             val alertDialogBuilder = AlertDialog.Builder(this)
             alertDialogBuilder.setTitle("入力エラー")
@@ -40,20 +43,32 @@ class MainActivity : AppCompatActivity() {
             return
         }
 
+        // 入力値がNULLでないとき、その値を設定
+        if (!inputHour.isNullOrEmpty()) {
+            setHour = inputHour.toInt()
+        }
+        if (!inputMinute.isNullOrEmpty()) {
+            setMinute = inputMinute.toInt()
+        }
+
         // TimePickerDialog表示
         val timePickerDialog = TimePickerDialog(
             this,
             TimePickerDialog.OnTimeSetListener { view, hour, minute ->
+                // あいさつ文言表示
                 greetResult.text = getGreetMessage(hour, minute)
+                // 入力値の表示更新
+                editHour.setText(hour.toString(), BufferType.NORMAL)
+                editMinute.setText(minute.toString(), BufferType.NORMAL)
             },
-            inputHour.toInt(), inputMinute.toInt(), true
+            setHour, setMinute, true
         )
         timePickerDialog.show()
     }
 
     // あいさつ文言取得
     private fun getGreetMessage(hour: Int, minute: Int): String {
-        var greetMessage:String
+        var greetMessage: String
 
         if (hour >= 2 && hour < 10) {
             greetMessage = "おはよう"
